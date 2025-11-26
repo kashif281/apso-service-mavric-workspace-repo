fb1516f7941fc5ec29e8cdbf9d97aef1e14e47cd
kashif281 <kashif281@example.com>
2025-11-26 13:21:47 +0000

    apso commit

--- src/app.module.rest.ts
+++ src/app.module.rest.ts
@@ -4,14 +4,12 @@ import { ConfigModule } from '@nestjs/config';
 import { typeOrmAsyncConfig } from './orm.config';
 import { HealthCheckController } from './healthCheck/HealthCheckController';
 import moduleImports from './autogen';
-import testmoduleImports from './test-module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
     ...moduleImports,
-    ...testmoduleImports,
   ],
   controllers: [HealthCheckController],
 })

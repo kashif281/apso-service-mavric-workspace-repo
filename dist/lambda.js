f6a03d6ecc8d94206147b95ad11a95ebb7e6f90f
Apso <apso-bot@mavric.com>
2025-11-26 11:41:59 +0000

    Auto-generate database migrations

--- dist/lambda.js
+++ dist/lambda.js
@@ -7,6 +7,18 @@ const app_module_rest_1 = require("./app.module.rest");
 const nest_app_config_1 = require("./config/nest-app.config");
 let cachedServer;
 const handler = async (event, context) => {
+    if (event.httpMethod === 'OPTIONS') {
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type,Authorization',
+                'Access-Control-Max-Age': '86400',
+            },
+            body: '',
+        };
+    }
     if (!cachedServer) {
         const nestApp = await core_1.NestFactory.create(app_module_rest_1.AppModule);
         await (0, nest_app_config_1.configureNestApp)(nestApp);
